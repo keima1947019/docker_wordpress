9671e6733ffdb4c92a91110feb126640ba1f0a3e
keima1947019 <cindyrotus@gmail.com>
2023-07-05 16:08:31 +0900

    Update 04.installingWpCli.sh

--- 04.installingWpCli.sh
+++ 04.installingWpCli.sh
@@ -1,3 +1,4 @@
+WPCON="<name of wordpress container>"
 echo "Installing WP-CLI... Requires PHP 5.6 or higher..."
-docker-compose exec kanepon curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-docker-compose exec kanepon chmod +x /usr/local/bin/wp
+docker exec -i $WPCON curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+docker exec -i $WPCON chmod +x /usr/local/bin/wp
