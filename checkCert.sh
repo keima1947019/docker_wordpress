c61a281b94b95ece633ab1c788f1898a7731ac41
keima1947019 <cindyrotus@gmail.com>
2023-07-05 16:11:07 +0900

    Delete checkCert.sh

--- checkCert.sh
+++
@@ -1,6 +0,0 @@
-CERT_KANEPON_COM="prx/crt/kanepon.com.crt"
-echo "kanepon.com"
-openssl x509 -text -noout -in ${CERT_KANEPON_COM} | grep Validity -A2
-echo "*.kanepon.com"
-CERT_WILDCARD_KANEPON_COM="prx/crt/wildcard.kanepon.com.crt"
-openssl x509 -text -noout -in ${CERT_WILDCARD_KANEPON_COM} | grep Validity -A2
